17971ec0444b935e5097e1e781d0eacf83383b74
William Ferreira <wqferr@gmail.com>
2024-01-06 10:05:06 -0300

    Create basic contact filter

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,29 @@
 
 class ContactListener: public b2ContactListener {
     void BeginContact(b2Contact *contact) {
-        ;
     }
 
     void EndContact(b2Contact *contact) {
+    }
+};
+
+class ContactFilter: public b2ContactFilter {
+    bool ShouldCollide(b2Fixture *fixtureA, b2Fixture *fixtureB) {
+        using Types = Entity::EntityType;
+        Entity *a = Entity::fromFixture(fixtureA);
+        Entity *b = Entity::fromFixture(fixtureB);
+
+        if (b->type == Types::PLAYER) {
+            std::swap(a, b);
+            std::swap(fixtureA, fixtureB);
+        }
+
+        // we dont want rockets colliding with players, as they would either or both
+        // push the player and explode immediately
+        if (a->type == Types::PLAYER && b->type == Types::ROCKET) {
+            return false;
+        }
+        return true;
     }
 };
 
@@ -17,13 +36,15 @@ int main(int argc, char *argv[]) {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Rocket Jump!");
     SetTargetFPS(60);
     float timeSlice = 0;
     ContactListener myContactListener;
+    ContactFilter myContactFilter;
 
     b2World world({0.0f, 10.0f});
     world.SetContactListener(&myContactListener);
+    world.SetContactFilter(&myContactFilter);
 
     auto player = Player(world, b2Vec2{0, 0});
     auto wall = Wall(world, b2Vec2{-10, 10}, b2Vec2{20, 5});
